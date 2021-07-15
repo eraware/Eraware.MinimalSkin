6a95df129cc229ac03cc3d3c4847b546edc5945e
Daniel Valadas <info@danielvaladas.com>
2021-07-14 21:00:46 -0400

    Implemented mouse support for the menu system
    Users can now either hover each menu or click on the chevron in order to expand each menu.
    
    Closes Add hover support to menu in desktop mode #326

--- src/scripts/menu/menu.ts
+++ src/scripts/menu/menu.ts
@@ -33,47 +33,77 @@ export default class Menu {
         });
 
         this.attachButtons();
+        
+        if (this.ul.parentElement?.id !== "main-menu" && this.ul.parentElement?.classList.contains("hasChildren")){
+            this.ul.parentElement.addEventListener("mouseenter", () => {
+                this.open();
+            });
+            
+            this.ul.parentElement.addEventListener("mouseleave", () => { 
+                this.close();
+            });
+        }
     }
 
     toggle() {
         if (this.isOpen){
-            this.button.parentElement?.classList.remove("expanded");
-            this.ul.style.height = "0";
-            const collapsed = new CustomEvent(
-                "collapsed",
-                {
-                    bubbles: true,
-                    detail: {
-                        change: this.ul.scrollHeight,
-                    },
-                });
-            this.ul.dispatchEvent(collapsed);
+            this.close();
         }
         else{
-            this.button.parentElement?.classList.add("expanded");
-            this.ul.style.height = this.ul.scrollHeight.toString() + "px";
-            var parentUl = this.ul.closest("ul");
-            if (parentUl){
-                parentUl.style.height = parentUl.scrollHeight + "px";
-            }
-            const expanded = new CustomEvent(
-                "expanded",
-                {
-                    bubbles: true,
-                    detail: {
-                        change: this.ul.scrollHeight,
-                    },
-                });
-            this.ul.dispatchEvent(expanded);
+            this.open();
         }
+    }
 
-        this.isOpen = !this.isOpen;
+    private open() {
+        this.closeOthers();
+        this.button.parentElement?.classList.add("expanded");
+        this.ul.style.height = this.ul.scrollHeight.toString() + "px";
+        var parentUl = this.ul.closest("ul");
+        if (parentUl) {
+            parentUl.style.height = parentUl.scrollHeight + "px";
+        }
+        const expanded = new CustomEvent(
+            "expanded",
+            {
+                bubbles: true,
+                detail: {
+                    change: this.ul.scrollHeight,
+                },
+            });
+        this.ul.dispatchEvent(expanded);
+        this.isOpen = true;
+    }
+
+    private close() {
+        this.button.parentElement?.classList.remove("expanded");
+        this.ul.style.height = "0";
+        const collapsed = new CustomEvent(
+            "collapsed",
+            {
+                bubbles: true,
+                detail: {
+                    change: this.ul.scrollHeight,
+                },
+            });
+        this.ul.dispatchEvent(collapsed);
+        this.isOpen = false;
+    }
+
+    closeOthers() {
+        const nav = this.ul.closest("nav");
+        const expandedItems = nav?.querySelectorAll(".menu-item.expanded");
+        expandedItems?.forEach(expandedItem => {
+            if (!expandedItem.contains(this.ul)){
+                const button = expandedItem.querySelector("button");
+                button?.click();
+            }
+        })
     }
 
     attachButtons() {
         const children = this.ul.children;
         for (let index = 0; index < children.length; index++) {
-            const li = children[index];
+            const li = children[index] as HTMLLIElement;
             if (li.classList.contains("hasChildren")){
                 const ul = li.querySelector("ul") as HTMLUListElement;
                 const button = li.querySelector("button") as HTMLButtonElement;
